e7530eee63d2f02acb49871093697eb67b3155e0
Flossie38 <fleur.jarman@gmail.com>
2020-11-25 11:50:46 +0000

    added all required page templates and updated end points in run.py file

--- run.py
+++ run.py
@@ -12,20 +12,35 @@ def index():
     return render_template("index.html", page_title="Choose from the below options")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html", page_title="Search a recipe")
+@app.route("/search")
+def search():
+    return render_template("search.html", page_title="Search a recipe")
 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html", page_title="Share your Yummy!")
+@app.route("/share")
+def share():
+    return render_template("share.html", page_title="Share your Yummy!")
 
 
-@app.route("/careers")
-def careers():
-    return render_template("careers.html")
-    
+@app.route("/breakfast")
+def breakfast():
+    return render_template("breakfast.html", page_title="Wholesome breakfasts!")
+
+
+@app.route("/mains")
+def mains():
+    return render_template("mains.html", page_title="Luscious mains!")
+
+
+@app.route("/dessert")
+def dessert():
+    return render_template("dessert.html", page_title="Decadent desserts!")
+
+
+@app.route("/nibble")
+def nibble():
+    return render_template("nibble.html", page_title="Nibble and snacks")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
