f53138a3e2dc916f7c076af45c8c4ebbefc7d036
Flossie38 <fleur.jarman@gmail.com>
2020-11-26 15:43:26 +0000

    revised search buttons and added log in or register page

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import os
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -7,9 +10,20 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.SECRET_KEY = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 @app.route("/")
 def index():
     return render_template("index.html", page_title="Choose from the below options")
+
+
+@app.route("/resister", methods=["GET", "POST"])
+def register():
+    return render_template("register.html", page_title="Log in or Register")
 
 
 @app.route("/search")
