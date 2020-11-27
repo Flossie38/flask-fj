64d381876a93e01c07411fb66f7317737012237a
Flossie38 <fleur.jarman@gmail.com>
2020-11-27 13:21:08 +0000

    updated secret key, added flash messaging and css override file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template
+from flask import (
+    Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -12,17 +13,43 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.SECRET_KEY = os.environ.get("SECRET_KEY")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
+
+
 
 @app.route("/")
 def index():
     return render_template("index.html", page_title="Choose from the below options")
 
 
-@app.route("/resister", methods=["GET", "POST"])
+@app.route("/recipes")
+def recipes():
+    recipes = mongo.db.recipes.find()
+    return render_template("recipes.html", recipes=recipes)
+
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        #CHECK IF USER EXISTS
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("  Ooops! That Username already exists - Please choose another  ")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        #put new user into session cookie
+        session["user"] = request.form.get("username").lower()
+        flash("You are registered!")
     return render_template("register.html", page_title="Log in or Register")
 
 
